3b0ad6e71e005485569075256b09397123bdd372
Kied Llaentenn <32681240+Kiedtl@users.noreply.github.com>
2019-11-16 11:16:05 -0500

    add parser.rs

---
+++ src/parser.rs
@@ -0,0 +1,81 @@
+use std::vec::Vec;
+
+pub enum TokenType {
+    Address,
+    Register,
+    StackTop,
+    StackBottom,
+    RawValue,
+}
+
+pub struct Token {
+    pub token:      String,
+    pub token_type: TokenType,
+}
+
+pub struct Command {
+    pub command:    &'static str,
+    pub arguments:  Vec<Token>,
+    pub trailer:    &'static str, // any random chars that hang out at the end of a command. e.g. ] or )
+}
+
+// match token type
+fn match_token(_char: char) -> TokenType {
+    match _char {
+        '~' => TokenType::Address,
+        '%' => TokenType::Register,
+        '@' => TokenType::StackTop,
+        '!' => TokenType::StackBottom,
+        '_' => TokenType::RawValue,
+    }
+}
+
+// parse individual commands
+pub fn parse(command: String) -> Command {
+    let chars = command.chars().collect::<Vec<char>>();
+    let arguments = Vec::<Token>::new();
+    let trailer: &str = if chars[chars.len()-1] == ']' ||
+        chars[chars.len()-1] == ')' 
+        {
+            &chars[chars.len()-1].to_string()
+        } else {
+            ""
+        };
+    let mut ctr = 1;
+    for _ in 1..chars.len() {
+        let mut c_char = chars[ctr];
+        if is_tokentype(c_char) {
+            let mut tokbuf = Token { token: "".to_owned(), token_type: match_token(c_char) };
+            ctr = ctr + 1;
+            if ctr == chars.len() {
+                break;
+            }
+            c_char = chars[ctr];
+
+            while !is_tokentype(c_char) {
+                tokbuf.token.push(c_char);
+                if ctr == chars.len() {
+                    break;
+                }
+            }
+            arguments.push(tokbuf);
+        }
+        ctr = ctr + 1;
+    }
+    Command {
+        command: &chars[0].to_string(),
+        arguments: arguments,
+        trailer: trailer,
+    }
+}
+
+fn is_tokentype(_char: char) -> bool {
+    match _char {
+        '~'|
+            '%'|
+            '@'|
+            '!'|
+            '_' => true,
+        _ => false,
+    }
+}
